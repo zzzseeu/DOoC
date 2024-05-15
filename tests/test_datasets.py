fcf08b6db83fedd4910aa17665972060c4175d4c
zhouxin <zhouxin@ish.org.cn>
2024-05-15 09:48:42 +0000

    feat: test
    fix: .github/workflow
    
    fix: pr comments
    
    fix: del model.eval() and .forward
    
    fix: del model.eval() and .forward
    
    fix: test_models load model
    
    fix: test_pipelines model init

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,17 +1,49 @@
 import pytest
 from dooc import datasets
+import random
+from moltx import tokenizers as tkz
+from moltx.models import AdaMRTokenizerConfig
 
 
 def test_MutSmi():
-    ds = datasets.MutSmi()
-    return
-    
-def test_MutSmiXAttention():
-    ds = datasets.MutSmiXAttention()
-    entry = ds(["CC[N+](C)(C)Cc1ccccc1Br"], [[1, 0, 1]], [0.85])
     return
 
+
+# def test_MutSmiXAttention():
+#     tokenizer = tkz.MoltxTokenizer.from_pretrain(
+#         conf=AdaMRTokenizerConfig.Prediction
+#         )
+#     ds = datasets.MutSmiXAttention(tokenizer)
+#     smiles = ["CC[N+]CCBr", "Cc1ccc1"]
+#     values = [0.88, 0.89]
+#     mutations = [[random.choice([0, 1]) for _ in range(3008)],
+#                  [random.choice([0, 1]) for _ in range(3008)]]
+#     with pytest.raises(RuntimeError):
+#         ds(smiles, mutations, values[:1])
+#     smiles_src, smiles_tgt, mutations_src, out = ds(smiles,
+#                                                     mutations,
+#                                                     values)
+#     assert smiles_src.shape == (2, 4)
+#     assert smiles_tgt.shape == (2, 6)
+#     assert mutations_src.shape == (2, 3008)
+#     assert out.shape == (2, 1)
+
+
 def test_MutSmiFullConnection():
-    ds = datasets.MutSmiFullConnection()
-    entry = ds(["CC[N+](C)(C)Cc1ccccc1Br"], [[1, 0, 1]], [0.85])
-    return
+    tokenizer = tkz.MoltxTokenizer.from_pretrain(
+        conf=AdaMRTokenizerConfig.Prediction
+        )
+    ds = datasets.MutSmiFullConnection(tokenizer)
+    smiles = ["CC[N+]CCBr", "Cc1ccc1"]
+    values = [0.88, 0.89]
+    mutations = [[random.choice([0, 1]) for _ in range(3008)],
+                 [random.choice([0, 1]) for _ in range(3008)]]
+    with pytest.raises(RuntimeError):
+        ds(smiles, mutations, values[:1])
+    smiles_src, smiles_tgt, mutations_src, out = ds(smiles,
+                                                    mutations,
+                                                    values)
+    assert smiles_src.shape == (2, 4)
+    assert smiles_tgt.shape == (2, 6)
+    assert mutations_src.shape == (2, 3008)
+    assert out.shape == (2, 1)
